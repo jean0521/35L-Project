05c133ed2381310d764fee81da58451cc03d9e4a
yangweng <50976865+realyangweng@users.noreply.github.com>
2024-02-10 00:28:04 -0800

    Update aside.component.js

--- chat_room_react/src/views/home/components/aside.component.js
+++ chat_room_react/src/views/home/components/aside.component.js
@@ -1,1 +1,8 @@
-先占坑
+import React, { useState, useEffect, useRef } from "react";
+import { List, Popconfirm, Badge, Modal, Button, message, Input } from "antd";
+import Draggable from "react-draggable";
+import { useNavigate, useParams } from "react-router-dom";
+import api from "../../../http/api";
+const { Search } = Input;
+
+没写完
