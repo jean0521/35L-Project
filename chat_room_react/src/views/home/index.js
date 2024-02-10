c47dc3d469cfd9cba4e3c4fb18a1ee7934ca6726
yangweng <50976865+realyangweng@users.noreply.github.com>
2024-02-10 00:26:58 -0800

    Update index.js

--- chat_room_react/src/views/home/index.js
+++ chat_room_react/src/views/home/index.js
@@ -1,1 +1,10 @@
-先占坑
+import React, { useContext, useEffect ,useState,useRef} from "react";
+// 调用 mock api
+import "./style.css";
+import Draggable from "react-draggable";
+import AsideComponent from "./components/aside.component";
+import MsgComponent from "./components/msg.component";
+import ChatContext from "../../context/index";
+import { Button ,message,Modal,List} from "antd";
+
+没写完
