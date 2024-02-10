a37a866a4abfaa77a80fc7ff5efa61f8b56e1d2d
yangweng <50976865+realyangweng@users.noreply.github.com>
2024-02-10 00:29:38 -0800

    Update index.js

--- chat_room_react/src/views/user/index.js
+++ chat_room_react/src/views/user/index.js
@@ -1,1 +1,10 @@
-先占坑
+import React from "react";
+import "./style.css";
+import { Outlet } from "react-router-dom";
+export default function User() {
+  return (
+    <div className=" containerLogin">
+      <Outlet key="outlet" />
+    </div>
+  );
+}
