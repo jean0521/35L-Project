32d90521c6f5d808c3fc3d3d96ac0ead9c387740
yangweng <50976865+realyangweng@users.noreply.github.com>
2024-02-13 16:52:06 -0800

    Update route.js

--- chat_room_react/src/router/route.js
+++ chat_room_react/src/router/route.js
@@ -1,1 +1,7 @@
-先占坑
+import { Navigate } from 'react-router-dom'
+import Home from '../views/home/index';
+import UserLogin from '../views/user/login';
+import UserRegister from '../views/user/register';
+import Chat from '../views/chat';
+
+没写完
