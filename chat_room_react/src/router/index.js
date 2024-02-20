7a9287a68eb77cddbe9e635fbfbe5ff6e5033c93
fffaa7788 <156288792+fffaa7788@users.noreply.github.com>
2024-02-19 16:39:10 -0800

    Update index.js

--- chat_room_react/src/router/index.js
+++ chat_room_react/src/router/index.js
@@ -2,4 +2,26 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import PrivateRoute from "../components/PrivateRoute";
 
-没写完
+const DomTitle = ({ route }) => {
+  document.title = route.meta.title;
+  return route.meta.isLogin ? (
+    <PrivateRoute element={route.element} userRole={route.meta} />
+  ) : (
+    route.element
+  );
+};
+function RouterList(props) {
+  return (
+    <Routes>
+      {props.data.map((route, index) => (
+        <Route
+          key={index}
+          path={route.path}
+          element={<DomTitle route={route} />}
+        />
+      ))}
+    </Routes>
+  );
+}
+
+export default RouterList;
