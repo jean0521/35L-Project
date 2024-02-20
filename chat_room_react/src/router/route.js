3153cf72f973f8fcef5a06e25cf11d14b51ffe06
fffaa7788 <156288792+fffaa7788@users.noreply.github.com>
2024-02-19 16:41:18 -0800

    Update route.js

--- chat_room_react/src/router/route.js
+++ chat_room_react/src/router/route.js
@@ -4,4 +4,49 @@ import UserLogin from '../views/user/login';
 import UserRegister from '../views/user/register';
 import Chat from '../views/chat';
 
-没写完
+const routesList = [
+    {
+        path: '/home/:id?',
+        element: <Home />,
+        meta:{
+            title: 'Homepage',   //首页
+            isLogin: true
+        },
+        children: []
+    },
+    {
+        path: '/Chat',
+        element: <Chat />,
+        meta:{
+            title: 'Homepage',   //首页
+            isLogin: true
+        },
+        children: []
+    },
+    {
+        path: '/',
+        element: <Navigate to="/home" />,
+        meta:{
+            title: 'Homepage',   //首页
+            isLogin: false
+        },
+    },
+    {
+        path: '/user/login',
+        meta:{
+            title: 'User-Login',   //用户-登录
+            isLogin: false
+        },
+        element: <UserLogin />
+    },
+    {
+        path: '/user/register',
+        meta:{
+            title: 'User-Register',   //用户-注册
+            isLogin: false
+        },
+        element: <UserRegister />
+    },
+]
+
+export default routesList;
