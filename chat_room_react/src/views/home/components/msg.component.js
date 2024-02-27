9bc2305485e3a1454387037c2c0dda8cf1d5ee31
yangweng <50976865+realyangweng@users.noreply.github.com>
2024-02-27 03:13:06 -0800

    Update msg.component.js

--- chat_room_react/src/views/home/components/msg.component.js
+++ chat_room_react/src/views/home/components/msg.component.js
@@ -1,5 +1,165 @@
 import React, { useEffect, useState, useRef } from "react";
 import { message, Button, Popconfirm } from "antd";
 import SendComponent from './send.component'
+export default function MsgComponent(props) {
+  const messagesRef = useRef();
+  const [messageApi, contextHolder] = message.useMessage();
+  const [msgList, setMsgList] = useState([]);
+  useEffect(() => {
+    const handleMessage = (msg) => {
+      console.log("Received message:", msg);
+      setMsgList((prevMsgList) => [
+        ...prevMsgList,
+        {
+          id: prevMsgList.length + 1,
+          userId: msg.from,
+          friendId: props.data.chatData.user.id,
+          content: msg.content,
+          createdAt: times(),
+          add: 1
+        },
+      ]);
+      setTimeout(() => {
+        messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+      }, 200)
+    };
+    if (props.data.chatData.friend) {
+      getChatHistory()
+    }
+    props.data.socket.on("receiveMessage", handleMessage);
 
-没写完
+    // 清理函数
+    return () => {
+      props.data.socket.off("receiveMessage", handleMessage);
+    };
+  }, [props]);
+
+  const getChatHistory = () => {
+    props.data.socket.emit(
+      "getChatHistory",
+      { userId: props.data.chatData.user.id, friendId: props.data.chatData.friend },
+      (res) => {
+        if (res.code === 0) {
+          setMsgList(res.data)
+          setTimeout(() => {
+            messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+          }, 200)
+        }
+        console.log("Received message:", res);
+      }
+    );
+  }
+  const times = () => {
+    const currentDate = new Date();
+    const hours = currentDate.getHours().toString().padStart(2, '0'); // 小时，确保是两位数
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0'); // 分钟，确保是两位数
+    const seconds = currentDate.getSeconds().toString().padStart(2, '0'); // 秒，确保是两位数
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    return currentTime;
+}
+  const deleteUser = async (e) => {
+    try {
+      console.log({ userId: props.data.chatData.user.id, friendId: props.data.chatData.friend, socketId: props.data.chatData.socketId })
+      props.data.socket.emit('delFriends', { userId: props.data.chatData.user.id, friendId: props.data.chatData.friend, socketId: props.data.chatData.socketId }, (result) => {
+        if (result.code === 0) {
+          props.data.setChatData((prevChatData) => ({
+            ...prevChatData,
+            friend: null,
+            socketId: null,
+            username: null,
+          }));
+        }
+        console.log(result)
+        messageApi.open({
+          type: result.code === 0 ? "success" : "error",
+          content: result.msg,
+        });
+      })
+    } catch (error) {
+      messageApi.open({
+        type: "error",
+        content: JSON.stringify(error),
+      });
+    }
+  };
+
+  const cancel = (e) => {
+    console.log(e);
+    messageApi.open({
+      type: "info",
+      content: 'Undo Deletion',     // 取消删除
+    });
+  };
+
+
+  // 接受发送消息
+  const sendMsg = (msg) => {
+    console.log(msg)
+    if (msg.success) {
+      setMsgList((prevMsgList) => [
+        ...prevMsgList,
+        {
+          id: prevMsgList.length + 2,
+          ...msg
+        }
+      ]);
+      if (msg.add === 1) {
+        setTimeout(() => {
+          messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+        }, 200)
+      }
+    }
+  }
+
+  return (
+    <div className="msg_list">
+      {contextHolder}
+      {/* {JSON.stringify(props.data.chatData)} */}
+      <div className="msgBox" ref={messagesRef}>
+        <div className="header">
+          {
+            props.data.chatData.username ?
+              <>
+                <p>用户：{props.data.chatData.username}</p>
+                <Popconfirm
+                  title="Delete the friend?"    // 删除好友？
+                  onConfirm={deleteUser}
+                  onCancel={cancel}
+                  okText="OK"       // 确认
+                  cancelText="Undo"  // 取消
+                >
+                  <Button type="primary" danger>    // 删除好友
+                    DeleteFriend
+                  </Button>
+                </Popconfirm>
+              </>
+              : null
+          }
+
+        </div>
+        {props.data.chatData?.friend ? (
+          msgList.length <= 0 ? (
+            <div className="msgxt msgx">No Messages</div>    // 暂无消息
+          ) : (
+            msgList.map((item, index) => {
+              return item.userId !== props.data.chatData.user.id ? (
+                <div className="msgxt msgx" key={index + "you-msg"}>
+                  <div className="msgName">{item.content}</div>
+                  <div className="msgTime">{item?.add === 1 ? item.createdAt : new Date(item.createdAt).toLocaleTimeString('en-US', { timeZone: 'America/Los Angeles', hour12: false })}</div>
+                </div>
+              ) : (
+                <div className="msgxy msgx" key={index + "my-msg"}>
+                  <div className="msgName">{item.content}</div>
+                  <div className="msgTime">{item?.add === 1 ? item.createdAt : new Date(item.createdAt).toLocaleTimeString('en-US', { timeZone: 'America/Los Angeles', hour12: false })}</div>
+                </div>
+              );
+            })
+          )
+        ) : (
+          <>Lets Chat! </>  // 跟好友聊聊吧！
+        )}
+      </div>
+      <SendComponent sendMsg={sendMsg} data={props.data} />
+    </div>
+  );
+}
