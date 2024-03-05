04cc3ca43ba9a618e7f5f6fcfbd755d649338474
fffaa7788 <156288792+fffaa7788@users.noreply.github.com>
2024-03-04 20:09:13 -0800

    Update index.js

--- chat_room_react/src/http/index.js
+++ chat_room_react/src/http/index.js
@@ -1,1 +1,21 @@
-先占坑
+import axios from 'axios';
+// 创建一个 Axios 实例
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+  timeout: 10000, // 设置请求超时时间
+});
+
+// 拦截请求，设置请求头等信息
+instance.interceptors.request.use(
+  (config) => {
+    // 在这里添加请求头
+    config.headers['Content-Type'] = 'multipart/form-data';
+    config.headers['authorization'] = window.localStorage.getItem('user') ? JSON.parse(window.localStorage.getItem('user')).authorization || '' : '';
+    // 可以添加其他请求头或修改配置
+    return config;
+  },
+  (error) => {
+    // 请求错误处理
+    return Promise.reject(error);
+  }
+);
