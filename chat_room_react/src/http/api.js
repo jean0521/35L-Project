06f256d209d6e6553df75fb374db6e140e1094c4
Sapphire <sapphireyq@gmail.com>
2024-03-10 01:28:05 -0800

    Update api.js

--- chat_room_react/src/http/api.js
+++ chat_room_react/src/http/api.js
@@ -1,20 +1,23 @@
-import http from "./index"
-//定义一个对象，用来存放所有的接口
-const api={
-  //注册
-  register:(data)=> http.post("/users/register", data),
+import http from "./index";
+
+// 定义一个对象，用来存放所有的接口
+const api = {
   // 注册
-  login:(data)=> http.post("/users/login", data),
-  //获取好友列表
-  getFriendsList:(data)=> http.post("/users/getFriendsList", data),
+  register: (data) => http.post("/users/register", data),
+  // 注册
+  login: (data) => http.post("/users/login", data),
+  // 获取好友列表
+  getFriendsList: (data) => http.post("/users/getFriendsList", data),
   // 搜索好友
-  findFriend:(data)=> http.post("/users/findFriend", data),
-  //申请好友
-  addFriend:(data)=> http.post("/users/addFriend", data),
+  findFriend: (data) => http.post("/users/findFriend", data),
+  // 申请好友
+  addFriend: (data) => http.post("/users/addFriend", data),
   // 删除好友
-  delFriend:(data)=> http.post("/users/delFriend", data),
-  //发送消息
-  sendMsg:(data)=> http.post("/msg/sendMsg", data),
-}
+  delFriend: (data) => http.post("/users/delFriend", data),
+  // 发送消息
+  sendMsg: (data) => http.post("/msg/sendMsg", data),
+
+};
+
 // 导出 api 对象
 export default api;
